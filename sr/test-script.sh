93fa9fb38738a4cf11aae968a084245a4857fea0
Rob Bos <rajbos@users.noreply.github.com>
2022-06-27 12:04:56 +0200

    Let the job run for an hour and ten minutes to be more useful

--- sr/test-script.sh
+++ sr/test-script.sh
@@ -4,10 +4,13 @@ echo "Hello world"
 sleep 10s
 echo "Wake up!"
 
-for i in {1..480}
+for a in {1..35}
 do
-  echo "$i, some data for the log line so that we store some text in the logs :-D"
-  sleep .25s
+ for i in {1..480}
+ do
+   echo "$i, some data for the log line so that we store some text in the logs :-D"
+   sleep .25s
+ done
 done
 
 echo "Completed"
